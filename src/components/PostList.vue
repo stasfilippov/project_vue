e18e314e8a7fab9929162e9862362b2572fdfe98
stasfilippov <kron888999@gmail.com>
2023-05-24 00:33:17 +0300

    add: component-postItem

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,17 @@
 <template>
-    <div class="post" v-for="post in posts">
-        <div><strong>Название:</strong> {{ post.title }}</div>
-        <div><strong>Описание:</strong> {{ post.body }}</div>
+    <div>
+        <h3>Список пользователей</h3>
+        <post-item 
+            v-for="post in posts"
+            :post = 'post'
+            />
     </div>
 </template>
 
 <script>
+import PostItem from '@/components/PostItem.vue';
 export default {
+    components: {PostItem},
     props: { //компонент ожидает данные 
         posts: { // компонент ожидает посты
             type: Array, // тип получаемого props
