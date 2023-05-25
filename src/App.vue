004f57a6236e1e7b3bf631bffd8d1f7ca436efa2
stasfilippov <kron888999@gmail.com>
2023-05-25 22:37:36 +0300

    add: modal

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="app">
-    <post-form @create="createPost" />
+    <h1>Страница с постами</h1>
+    <input type="text" v-model.trim="modificatorValue" />
+    <my-button @click="showDialog" style="margin: 15px 0"
+      >Создать пост
+    </my-button>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form @create="createPost" />
+    </my-dialog>
     <post-list :posts="posts" @remove="removePost" />
   </div>
 </template>
@@ -8,9 +15,13 @@
 <script>
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
+import MyDialog from "@/components/UI/MyDialog.vue";
+import MyButton from "@/components/UI/MyButton.vue";
 
 export default {
   components: {
+    MyButton,
+    MyDialog,
     PostList,
     PostForm,
   },
@@ -23,14 +34,20 @@ export default {
         { id: 3, title: "JavaScript 3", body: "Описание поста 3" },
         { id: 4, title: "JavaScript 4", body: "Описание поста 4" },
       ],
+      dialogVisible: false,
+      modificatorValue: "",
     };
   },
   methods: {
     createPost(post) {
       this.posts.push(post);
+      this.dialogVisible = false;
     },
     removePost(post) {
       this.posts = this.posts.filter((p) => p.id !== post.id);
+    },
+    showDialog() {
+      this.dialogVisible = true;
     },
   },
 };
--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,8 +1,5 @@
-import MyButton from '@/components/UI/MyButton';
-import MyInput from '@/components/UI/MyInput';
+import MyButton from "@/components/UI/MyButton";
+import MyInput from "@/components/UI/MyInput";
+import MyDialog from "@/components/UI/MyDialog.vue";
 
-
-export default [
-    MyButton,
-    MyInput
-]
+export default [MyButton, MyInput, MyDialog];
